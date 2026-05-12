bb93f9043889f906ad6571405f2cdac9d7567cdf
VibeDylan <dylanbourbotte62@icloud.com>
2026-05-12 16:58:41 +0200

    feat: add filterData function to filter animals by name pattern

---
+++ src/filter.js
@@ -0,0 +1,28 @@
+/**
+ * @typedef {{ name: string }} Animal
+ * @typedef {{ name: string, animals: Animal[] }} Person
+ * @typedef {{ name: string, people: Person[] }} Country
+ */
+
+/**
+ * Filters countries, people and animals by a pattern on animal names.
+ * Countries and people with no remaining animals are removed.
+ *
+ * @param {Country[]} data
+ * @param {string} pattern
+ * @returns {Country[]}
+ */
+function filterData(data, pattern) {
+    return data.map(country => ({
+        ...country,
+        people: country.people.map(person => ({
+            ...person,
+            animals: person.animals.filter(animal => animal.name.toLowerCase().includes(pattern.toLowerCase()))
+        })).filter(person => person.animals.length > 0)
+    }))
+    .filter(country => country.people.length > 0);
+}
+
+module.exports = {
+    filterData,
+}
