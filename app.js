48285c197a82817cbdfd1f138b54c9543d963b05
VibeDylan <dylanbourbotte62@icloud.com>
2026-05-12 17:02:05 +0200

    feat: update cli with previously created method

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const { data} = require('./data')
+const { data } = require('./data');
+const { filterData } = require('./src/filter');
+const { countData } = require('./src/count');
+const { filterAndCount } = require('./src/filterAndCount');
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 const filterArg = args.find(a => a.startsWith('--filter='));
 const hasCount = args.includes('--count');
 
-if(filterArg && hasCount) {
-    // TODO
-} else if(filterArg) {
-    // TODO
-} else if(hasCount) {
-    // TODO
+if (filterArg && hasCount) {
+  const pattern = filterArg.split('=')[1];
+  console.log(JSON.stringify(filterAndCount(data, pattern), null, 2));
+} else if (filterArg) {
+  const pattern = filterArg.split('=')[1];
+  console.log(JSON.stringify(filterData(data, pattern), null, 2));
+} else if (hasCount) {
+  console.log(JSON.stringify(countData(data), null, 2));
 } else {
-    // TODO
+  console.error('Usage: node app.js --filter=<pattern> | --count | --filter=<pattern> --count');
+  process.exit(1);
 }
